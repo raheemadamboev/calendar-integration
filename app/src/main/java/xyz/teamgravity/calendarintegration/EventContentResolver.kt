3a60725988f70177084e216162646459a406aeee
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-18 13:15:53 +0500

    get events by date function to query for specific date

--- app/src/main/java/xyz/teamgravity/calendarintegration/EventContentResolver.kt
+++ app/src/main/java/xyz/teamgravity/calendarintegration/EventContentResolver.kt
@@ -5,19 +5,20 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
 import android.provider.CalendarContract
+import java.util.*
 
+@SuppressLint("Range")
 class EventContentResolver(
     context: Context
 ) {
 
     companion object {
         val URI: Uri = Uri.parse("content://com.android.calendar/events")
-        val PERMISSION = Manifest.permission.READ_CALENDAR
+        const val PERMISSION = Manifest.permission.READ_CALENDAR
     }
 
     private val resolver = context.contentResolver
 
-    @SuppressLint("Range")
     fun getEvents(): HashSet<EventModel> {
         val events = hashSetOf<EventModel>()
 
@@ -29,6 +30,51 @@ class EventContentResolver(
                 CalendarContract.Events.DTSTART,
                 CalendarContract.Events.DTEND
             ), null, null, null
+        )
+
+        cursor?.let {
+            try {
+                if (it.count > 0) {
+                    while (it.moveToNext()) {
+                        val title = it.getString(it.getColumnIndex(CalendarContract.Events.TITLE)) ?: ""
+                        val description = it.getString(it.getColumnIndex(CalendarContract.Events.DESCRIPTION)) ?: ""
+                        val startDate = it.getLong(it.getColumnIndex(CalendarContract.Events.DTSTART))
+                        val endDate = it.getLong(it.getColumnIndex(CalendarContract.Events.DTEND))
+
+                        events.add(
+                            EventModel(
+                                title = title,
+                                description = description,
+                                startDate = startDate,
+                                endDate = endDate
+                            )
+                        )
+                    }
+                }
+            } catch (e: AssertionError) {
+                raheem(e.message)
+                cursor.close()
+            }
+        }
+
+        cursor?.close()
+        return events
+    }
+
+    fun getEventsByDate(date: Date) {
+        val events = hashSetOf<EventModel>()
+
+        val cursor = resolver.query(
+            URI,
+            arrayOf(
+                CalendarContract.Events.TITLE,
+                CalendarContract.Events.DESCRIPTION,
+                CalendarContract.Events.DTSTART,
+                CalendarContract.Events.DTEND
+            ),
+            CalendarContract.Events.DTSTART + " > ? AND " + CalendarContract.Events.DTSTART + " < ?",
+            arrayOf(Time.minDay(date.time).toString(), Time.maxDay(date.time).toString()),
+            null
         )
 
         cursor?.let {
---
+++ app/src/main/java/xyz/teamgravity/calendarintegration/Time.kt
@@ -0,0 +1,31 @@
+package xyz.teamgravity.calendarintegration
+
+import java.util.*
+
+object Time {
+    /**
+     * return minimum day for comparing time in millis for sql
+     */
+    fun minDay(time: Long): Long {
+        val c = Calendar.getInstance()
+        c.timeInMillis = time
+        c.add(Calendar.DATE, -1)
+        c.set(Calendar.HOUR_OF_DAY, 23)
+        c.set(Calendar.MINUTE, 59)
+        c.set(Calendar.SECOND, 59)
+        return c.timeInMillis
+    }
+
+    /**
+     * return maximum day for comparing time in millis for sql
+     */
+    fun maxDay(time: Long): Long {
+        val c = Calendar.getInstance()
+        c.timeInMillis = time
+        c.add(Calendar.DATE, 1)
+        c.set(Calendar.HOUR_OF_DAY, 0)
+        c.set(Calendar.MINUTE, 0)
+        c.set(Calendar.SECOND, 0)
+        return c.timeInMillis
+    }
+}
