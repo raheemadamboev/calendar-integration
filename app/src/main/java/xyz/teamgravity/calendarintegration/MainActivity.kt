f6a73ae04e24aed4d83ea25c7e6db020dcfdb43f
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-18 11:24:02 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/calendarintegration/MainActivity.kt
@@ -0,0 +1,13 @@
+package xyz.teamgravity.calendarintegration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
